cf3c56fd57e85393e601507ef76760e94afcd0ee
krunnal <gharrekrunnal@gmail.com>
2014-10-09 11:19:25 +0530

    a

--- static/js/demo_multistream.js
+++ static/js/demo_multistream.js
@@ -103,7 +103,7 @@ function addSrcButton(buttonLabel, videoId) {
 }
 
 function connect() {
-	//easyrtc.setSocketUrl(":8080");
+	easyrtc.setSocketUrl("//easyrtc-sixsteps.rhcloud.com:8080");
 	console.log("Initializing.");
     easyrtc.setRoomOccupantListener(convertListToButtons);
     easyrtc.connect("easyrtc.multistream", loginSuccess, loginFailure);
